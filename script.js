f22b1e67cfcb56cb7909c003f6e9ebb408de8413
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-09-15 01:15:14 +0600

    show time in second issue solved

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ const gameOver = () => {
   document.removeEventListener("keydown", typeController);
   // the current time is the finish time
   // so total time taken is current time - start time
-  const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const finishTime = new Date().getSeconds();
+  const timeTaken = (finishTime - startTime);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -117,7 +117,7 @@ const start = () => {
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
-      startTime = new Date().getTime();
+      startTime = new Date().getSeconds();
     }
     count--;
   }, 1000);
@@ -131,9 +131,7 @@ displayHistory();
 
 // Show typing time spent
 setInterval(() => {
-  const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
-
-
+  const currentTime = new Date().getSeconds();
+  const timeSpent = (currentTime - startTime);
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
