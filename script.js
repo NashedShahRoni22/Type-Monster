47211b07759dc46f8dac3aa13fec7b78aa5ea168
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-09-15 13:07:00 +0600

    histroy time issue fixed

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const gameOver = () => {
     <button onclick="closeModal()">Close</button>
   `;
 
-  addHistory(questionText, timeTaken, errorCount);
+  addHistory(questionText, totalTakenTime, errorCount);
 
   // restart everything
   startTime = null;
